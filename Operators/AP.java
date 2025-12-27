5c474b9bf93a3effececf96da8c55b78a4d74f03
Aathithya Pandiyan <aathithyapandiyan01@gmail.com>
2025-12-27 13:14:30 +0530

    dec27hw

---
+++ Operators/AP.java
@@ -0,0 +1,34 @@
+class AP 
+{
+	public static void main ( String [] args )
+	{
+     int A = 45;
+	 int B = 67;
+	 System.out.println ( " A + B " + ( A + B ) );
+	}
+		
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
